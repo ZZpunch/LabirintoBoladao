7d838ec89d31caf963bd0d3fb91c9c8a0071c53c
Joao Casagrande <joao.casagrande@enext.com.br>
2020-05-25 23:13:27 -0300

    Adicionando boneco

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ GLfloat lmodel_ambient[] = { 0.6,0.6,0.6,1.0 };
 GLfloat mapaChegada[] = { 0.7, 0.1, 0.1, 1.0 };
 
 GLfloat jog_x = TAM_BLOCO, jog_z = TAM_BLOCO;
-GLfloat jog_y = 25;
+GLfloat jog_y = 0;
 GLfloat mov_x = 10, mov_y = 0, mov_z = 0;
 GLint angle = 0;
 GLint wire = 0;
@@ -60,6 +60,31 @@ int pode_mover(float pos_x, float pos_z, float mov_x, float mov_z)
 	else return 1;
 }
 
+void drawSnowMan() {
+	glColor3f(1.0f, 1.0f, 1.0f);
+// Draw Body
+	glTranslatef(0.0f ,0.75f, 0.0f);
+	glutSolidSphere(0.75f,20,20);
+
+
+// Draw Head
+	glTranslatef(0.0f, 1.0f, 0.0f);
+	glutSolidSphere(0.25f,20,20);
+
+// Draw Eyes
+	glPushMatrix();
+	glColor3f(0.0f,0.0f,0.0f);
+	glTranslatef(0.05f, 0.10f, 0.18f);
+	glutSolidSphere(0.05f,10,10);
+	glTranslatef(-0.1f, 0.0f, 0.0f);
+	glutSolidSphere(0.05f,10,10);
+	glPopMatrix();
+
+// Draw Nose
+	glColor3f(1.0f, 0.5f , 0.5f);
+	glutSolidCone(0.08f,0.5f,10,2);
+}
+
 void display(void)
 {
 	int x, y;
@@ -70,21 +95,21 @@ void display(void)
 
 	glLoadIdentity();
 
-	gluLookAt(jog_x, jog_y, jog_z, jog_x + mov_x, jog_y+ mov_y, jog_z + mov_z, 0, 1, 0);
+
+	gluLookAt(jog_x + mov_x, jog_y + mov_y, jog_z + mov_z, jog_x, jog_y , jog_z, 0, 1, 0);
+	glPushMatrix();
+		glTranslatef(jog_x, jog_y-2.5, jog_z);
+		drawSnowMan();
+	glPopMatrix();	
 
 	glPushMatrix();
-
-	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corChao);
-
-	glBegin(GL_QUADS);
-
-	glVertex3f(-10000, -TAM_BLOCO / 2, -10000);
-	glVertex3f(-10000, -TAM_BLOCO / 2, 10000);
-	glVertex3f(10000, -TAM_BLOCO / 2, 10000);
-	glVertex3f(10000, -TAM_BLOCO / 2, -10000);
-
-	glEnd();
-
+		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corChao);
+		glBegin(GL_QUADS);
+		glVertex3f(-10000, -TAM_BLOCO / 2, -10000);
+		glVertex3f(-10000, -TAM_BLOCO / 2, 10000);
+		glVertex3f(10000, -TAM_BLOCO / 2, 10000);
+		glVertex3f(10000, -TAM_BLOCO / 2, -10000);
+		glEnd();
 	glPopMatrix();
 
 	
@@ -99,19 +124,14 @@ void display(void)
 				y_mun = y * TAM_BLOCO;
 
 				glPushMatrix();
-				glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corParede);	
-
-				glTranslatef(x_mun, 5, y_mun);
-
-				if (wire) glutWireCube(TAM_BLOCO);
-				else glutSolidCube(TAM_BLOCO);
-
+					glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corParede);	
+					glTranslatef(x_mun, 5, y_mun);
+					if (wire) glutWireCube(TAM_BLOCO);
+					else glutSolidCube(TAM_BLOCO);
 				glPopMatrix();
-
 			}
 		}
 	}
-
 	glutSwapBuffers();
 }
 
@@ -130,18 +150,18 @@ void Keyboard_Function(unsigned char key, int x, int y)
 		case 'e':
 			jog_y -= 10;
 			break;
-		case 's':
+		case 'w':
 			if (pode_mover(jog_x, jog_z, -nextMoveX, -nextMoveZ))
 			{
 				jog_x -= nextMoveX;
 				jog_z -= nextMoveZ;	
 			}
 			break;
-		case 'w':
+		case 's':
 			if (pode_mover(jog_x, jog_z, nextMoveX, nextMoveZ))
 			{
-				jog_x += sinf(anglesX) * -cosf(anglesY) * PASSO;
-				jog_z += cosf(anglesX) * cosf(anglesY) * PASSO;
+				jog_x += nextMoveX;
+				jog_z += nextMoveZ;
 			}
 			break;
 	}
@@ -180,7 +200,7 @@ void mouseMove(int x, int y) {
 			anglesY = M_PI / 2;
 
 		mov_x = sinf(anglesX) * -cosf(anglesY);
-		mov_y = -sinf(anglesY);
+		mov_y = sinf(anglesY);
 		mov_z = cosf(anglesX) * cosf(anglesY);
 		
 		wrap = 1;
